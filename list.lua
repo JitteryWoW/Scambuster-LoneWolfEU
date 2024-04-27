a5660242959c6e08d8d9810945ee3db7b24f6962
Cei <123380487+CeiCode@users.noreply.github.com>
2024-04-27 20:44:16 +0200

    Update list.lua

---
+++ list.lua
@@ -0,0 +1,27 @@
+local _, t = ...
+
+-- Lone Wolf Discord Blocklist Table
+
+-- unique characters: 2
+
+t.version = "@project-version@"
+
+t.case_table = {
+[1] = {
+    name = "Test",
+    faction = "Alliance",
+    guid = "Player-5828-000000000",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.gg/BhcDCGeDSD",
+},
+
+[2] = {
+    name = "Test",
+    faction = "Horde",
+    guid = "Player-5828-000000001",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.gg/BhcDCGeDSD",
+},
+
+}
+
