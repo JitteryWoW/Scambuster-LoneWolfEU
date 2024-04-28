07ed3160e4d94f319095fe02e8484de6c0995c36
JitteryWoW <168013456+JitteryWoW@users.noreply.github.com>
2024-04-28 17:48:14 +0100

    Update list.lua
    Added Alliance Phase 3 blacklisted players.

--- list.lua
+++ list.lua
@@ -8,20 +8,43 @@ t.version = "@project-version@"
 
 t.case_table = {
 [1] = {
-    name = "Test",
+    name = "Schwurbla",
     faction = "Alliance",
-    guid = "Player-5828-000000000",
+    guid = "Player-5826-023A887A",
     description = "Ninja Looting - Raid",
-    url = "https://discord.gg/BhcDCGeDSD",
+    url = "https://discord.com/channels/1179118983518109836/1228501514008395917",
 },
 
 [2] = {
-    name = "Test",
-    faction = "Horde",
-    guid = "Player-5828-000000001",
+    name = "Ryanfingern",
+    faction = "Alliance",
+    guid = "Player-5826-026D247A",
     description = "Ninja Looting - Raid",
-    url = "https://discord.gg/BhcDCGeDSD",
+    url = "https://discord.com/channels/1179118983518109836/1230640445470544004",
 },
 
+[3] = {
+    name = "Frnki",
+    faction = "Alliance",
+    guid = "Player-5826-022A31B8",
+    description = "Scamming - Boost",
+    url = "https://discord.com/channels/1179118983518109836/1231607523644149831",
+},
+
+[4] = {
+    name = "Steelbane",
+    faction = "Alliance",
+    guid = "Player-5826-026FA34C",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1230822970063196191",
+},
+
+[5] = {
+    name = "Lemo",
+    faction = "Alliance",
+    guid = "Player-5826-027319DC",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1231671554828537909",
+},
 }
 
