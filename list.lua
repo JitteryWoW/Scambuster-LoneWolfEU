78eb65a60579527800ffcea80d294bdd55e60d0a
JitteryWoW <168013456+JitteryWoW@users.noreply.github.com>
2024-04-28 18:15:05 +0100

    Update list.lua
    Added horde phase 3 players

--- list.lua
+++ list.lua
@@ -46,5 +46,101 @@ t.case_table = {
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1179118983518109836/1231671554828537909",
 },
+    
+[6] = {
+    name = "Watershield",
+    faction = "Horde",
+    guid = "Player-5826-022B7BCC",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1224099461102309527",
+},
+    
+[7] = {
+    name = "Madncure",
+    faction = "Horde",
+    guid = "Player-5826-02472E58",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1224099461102309527",
+},
+    
+[8] = {
+    name = "Stormzz",
+    faction = "Horde",
+    guid = "Player-5826-0212EFC7",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1227001080915824860",
+},
+    
+[9] = {
+    name = "Tacodennis",
+    faction = "Horde",
+    guid = "Player-5826-01FA9C43",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1228517455195013120",
+},
+    
+[10] = {
+    name = "Tomspike",
+    faction = "Horde",
+    guid = "Player-5826-02564ADC",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1228517455195013120",
+},
+        
+[10] = {
+    name = "Elfatty",
+    faction = "Horde",
+    guid = "Player-5826-026A17CC",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1229487015221788776",
+},
+        
+[11] = {
+    name = "Dally",
+    faction = "Horde",
+    guid = "Player-5826-0205F912",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1230601820871856188",
+},
+            
+[12] = {
+    name = "Lunaire",
+    faction = "Horde",
+    guid = "Player-5826-0240EE7D",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1231075236707827722",
+},
+            
+[13] = {
+    name = "Rocksta",
+    faction = "Horde",
+    guid = "Player-5826-0272D584 ",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1231187688485425183",
+},
+            
+[14] = {
+    name = "Woottodo",
+    faction = "Horde",
+    guid = "Player-5826-022162A8",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1231187688485425183",
+},
+            
+[15] = {
+    name = "Mejin",
+    faction = "Horde",
+    guid = "Player-5826-02215A70",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1231187688485425183",
+},
+                
+[16] = {
+    name = "Blackwagyu",
+    faction = "Horde",
+    guid = "Player-5826-01FD62F8",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1179118983518109836/1233003919941304420",
+},
 }
 
